9f564e36f5e0cbce050987291f124868de12d0f3
Jimmy Gorecki <jimmy27.gorecki01@gmail.com>
2023-01-18 22:25:03 +0100

    maj footer + anim text header

--- javascript/animation.js
+++ javascript/animation.js
@@ -1,16 +1,25 @@
 window.onload = function() {
 
-text = document.getElementsByClassName("head_desc")[0];
-if(text) {
-    message = 'Jeune développeur web, sortant du lycée avec un bac technologique, j’ai décidé de continuer sur cette voie afin de m’orienter vers le développement web.\nJ’ai par la suite intégré la formation d’OpenClassrooms afin de réaliser mon objectif.\nJ’ai donc maintenant des connaissances sur plusieurs technologies telles que Html, Css, Javascript ou encore NodeJS. Au niveau de la gestion de projet je me suis familiarisé avec l’outil Git.\n\nJe suis actuellement à la recherche d’un poste de développeur front-end/React afin de me perfectionner et d’apprendre de nouvelles technologies.';
-    index = 0;
+    text = document.getElementsByClassName("head_desc")[0];
+    if(text) {
+        message = "Jeune développeur web, sortant du lycée avec un bac technologique, j’ai décidé de continuer sur cette voie afin de m’orienter vers le développement web.\nJ’ai par la suite intégré la formation d’OpenClassrooms afin de réaliser mon objectif.\nJ’ai donc maintenant des connaissances sur plusieurs technologies telles que Html, Css, Javascript ou encore NodeJS. Au niveau de la gestion de projet je me suis familiarisé avec l’outil Git.\n\nJe suis actuellement à la recherche d’un poste de développeur front-end/React afin de me perfectionner et d’apprendre de nouvelles technologies.";
+        index = 0;
+        cursor = '|';
+        let cursorInterval;
+
         function writeText() {
             text.innerHTML = message.substring(0, index);
             index++;
-            if (index <= message.length) {
-                setTimeout(writeText, 50);
+            if (index === message.length + 1) {
+                clearInterval(cursorInterval);
+            } else {
+                setTimeout(writeText, 30);
             }
         }
+
+        cursorInterval = setInterval(function() {
+            blinkCursor();
+        }, 200);
 
         function blinkCursor() {
             text.innerHTML += cursor;
@@ -19,10 +28,7 @@ if(text) {
             } else {
                 cursor = "|";
             }
-            setTimeout(blinkCursor, 400);
         }
-
         writeText();
-        blinkCursor();
     }
 };
