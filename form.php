b1e934afdcf8ba81308ea51c606a88ce152caef1
Jimmy Gorecki <jimmy27.gorecki01@gmail.com>
2023-02-05 01:28:16 +0100

    maj

---
+++ form.php
@@ -0,0 +1,25 @@
+<?php
+
+if(isset($_POST['nom']) && isset($_POST['email']) && isset($_POST['text'])){
+    $nom = $_POST['nom'];
+    $email = $_POST['email'];
+    $text = $_POST['text'];
+    
+    $to = "jimmy27.gorecki01@gmail.com";
+    $subject = "Nouveau message de votre site web";
+    $headers = "From: " . $email . "\r\n" .
+    "Reply-To: " . $email . "\r\n" .
+    "X-Mailer: PHP/" . phpversion();
+    $message = "Nom : " . $nom . "\n" . "Email : " . $email . "\n" . "Message : " . $text;
+    
+    if(mail($to, $subject, $message, $headers)){
+        echo "Message envoyé!";
+    } else {
+        echo "Une erreur est survenue, veuillez réessayer.";
+    }
+}
+
+?>
+
+
+
--- javascript/animation.js
+++ javascript/animation.js
@@ -2,7 +2,7 @@ window.onload = function() {
 
     text = document.getElementsByClassName("head_desc")[0];
     if(text) {
-        message = "Jeune développeur web, sortant du lycée avec un bac technologique, j’ai décidé de continuer sur cette voie afin de m’orienter vers le développement web.\nJ’ai par la suite intégré la formation d’OpenClassrooms afin de réaliser mon objectif.\nJ’ai donc maintenant des connaissances sur plusieurs technologies telles que Html, Css, Javascript ou encore NodeJS. Au niveau de la gestion de projet je me suis familiarisé avec l’outil Git.\n\nJe suis actuellement à la recherche d’un poste de développeur front-end/React afin de me perfectionner et d’apprendre de nouvelles technologies.";
+        message = "Jeune développeur web fraîchement diplômé, j’ai décidé d'intégrer la formation d’OpenClassrooms (Développeur web) dès la sortie du lycée afin de réaliser mon objectif.\nJ’ai donc maintenant des connaissances sur plusieurs technologies telles que Html, Css, Javascript ou encore NodeJS. Au niveau de la gestion de projet je me suis familiarisé avec l’outil Git.\n\nJe suis actuellement à la recherche d’un poste de développeur front-end/React afin de me perfectionner et d’apprendre de nouvelles technologies.";
         index = 0;
         cursor = '|';
         let cursorInterval;
@@ -13,7 +13,7 @@ window.onload = function() {
             if (index === message.length + 1) {
                 clearInterval(cursorInterval);
             } else {
-                setTimeout(writeText, 30);
+                setTimeout(writeText, 20);
             }
         }
 
