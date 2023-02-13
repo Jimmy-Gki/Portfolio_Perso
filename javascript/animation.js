03823c358a11e55d96f8de5b69e6c777d3904460
Jimmy Gorecki <jimmy27.gorecki01@gmail.com>
2023-02-13 12:13:24 +0100

    maj texte

--- javascript/animation.js
+++ javascript/animation.js
@@ -2,7 +2,7 @@ window.onload = function() {
 
     text = document.getElementsByClassName("head_desc")[0];
     if(text) {
-        message = "Jeune développeur web fraîchement diplômé, j’ai décidé d'intégrer la formation d’OpenClassrooms (Développeur web) dès la sortie du lycée afin de réaliser mon objectif.\nJ’ai donc maintenant des connaissances sur plusieurs technologies telles que Html, Css, Javascript ou encore NodeJS. Au niveau de la gestion de projet je me suis familiarisé avec l’outil Git.\n\nJe suis actuellement à la recherche d’un poste de développeur front-end/React afin de me perfectionner et d’apprendre de nouvelles technologies.";
+        message = "Développeur fraîchement diplômé, j’ai décidé d'intégrer la formation d’OpenClassrooms (Développeur web) dès la sortie du lycée afin de réaliser mon objectif.\nJ’ai donc maintenant des connaissances sur plusieurs technologies telles que Html, Css, Javascript ou encore NodeJS. Au niveau de la gestion de projet je me suis familiarisé avec l’outil Git.\n\nJe suis actuellement à la recherche d’un poste de développeur front-end/React afin de me perfectionner et d’apprendre de nouvelles technologies.";
         index = 0;
         cursor = '|';
         let cursorInterval;
