032918461a63376902de09d2f64205cbdb6c46a7
Jimmy Gorecki <jimmy27.gorecki01@gmail.com>
2023-02-23 01:50:31 +0100

    maj

--- form.php
+++ form.php
@@ -5,18 +5,27 @@ if(isset($_POST['nom']) && isset($_POST['email']) && isset($_POST['text'])){
     $email = $_POST['email'];
     $text = $_POST['text'];
     
-    $to_email = "jimmy27.gorecki01@gmail.com";
-    $subject = "Nouveau message de votre site web";
-    $headers = "From: " . $email . "\r\n" .
-    "Reply-To: " . $email . "\r\n" .
-    "X-Mailer: PHP/" . phpversion();
-    $message = "Nom : " . $nom . "\n" . "Email : " . $email . "\n" . "Message : " . $text;
-    
-    if(mail($to_email, $subject, $message, $headers)){
-        echo "Message envoyé!";
+    if(!empty($nom) && !empty($email) && !empty($text) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $to_email = "jimmy27.gorecki01@gmail.com";
+        $subject = "Nouveau message de votre site web";
+        $headers = "From: " . $email . "\r\n" .
+        "Reply-To: " . $email . "\r\n" .
+        "X-Mailer: PHP/" . phpversion();
+
+        ini_set("SMTP", "smtp.gmail.com");
+        ini_set("smtp_port", "587");
+        ini_set("smtp_crypto", "tls");
+
+        $message = "Nom : " . $nom . "\n" . "Email : " . $email . "\n" . "Message : " . $text;
+        
+        if(mail($to_email, $subject, $message, $headers)){
+            echo "Message envoyé!";
+        } else {
+            echo "Une erreur est survenue, veuillez réessayer.";
+        }
     } else {
-        echo "Une erreur est survenue, veuillez réessayer.";
-    }
+        echo "Veuillez remplir tous les champs et entrer une adresse email valide.";
+    } 
 }
 
 ?>
